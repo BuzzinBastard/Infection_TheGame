b47c1cfbc90d76b07bba19892ace71cf7ab50af2
Radish <wiensmarcus@gmail.com>
2020-09-27 13:15:47 -0500

    Add files via upload

--- Infection_Main.py
+++ Infection_Main.py
@@ -1,81 +1,30 @@
 #! /usr/bin/env python
 
+# Potential Bacteria Class Attributes
+# ATP (nrg to earn and spend), type, species, gram, motile, weapon, quorum, chemotaxis, defense, location, special, population size
+
+# Potential Immune system cell Class Atributes
 # Variables!
 game_Run = 1
 
-
-
-# Welcome
+# Welcome to the Game
 print("Congratulations and Welcome to Infection The Game!")
 
-#Introduction
-print("Welcome to the Tree of Life")
-print("Bacteria        Archaea             Eukarya")
-print("    \              |                   /   ")
-print("     \             |                 /    ")
-print("      \            |               /    ")
-print("       \           |             /    ")
-print("         \          |           /    ")
-print("          \         |         /    ")
-print("           \        |       /    ")
-print("             \      |     /    ")
-print("              \     |    |   ")
-print("               \    |   /  ")
-print("                \   |  /")
-print("                  \ | /  ")
-print("                    |   ")
-print("                    |   ")
-print("            Last Common Ancestor ")
-
+# Introduction
 # Character Setup including starting position, attributes of Class.
 # Think dictionaries for assigning things if dict(key) = value in dict.
 print("You awaken to find yourself freshly, wide-eyed daughter cell. Welcome to the world little one.")
 print("")
 print(" The next step is to get organized and figure out where you fit in and find your niche!")
 
-# User input will help create user microbe class. How to ensure user gives "useful" choices.
-type = "Bacteria"
-species = input("Please choose your species type: ")
-gram = input("Are you a gram negative or gram positive? ")
-motile = input("Is your organism Motile? ")
-weapon = input("Please choose your offensive advantage (weapon): ")
-quorum = input("Does your microbe have an ultimate (True or False)? ")
-chemotaxis = input("Can your microbe respond to stimulus (True or False)? ")
-defense = input("What is your main defense mechanism? ")
-location = input("What is your starting location? ")
-special = input("What is your secret special ability? ")
-
-# Create class with input variables.
-user = Microbe(25, type, species, gram, motile, weapon, quorum, chemotaxis, defense, location, special, 1)
-
 # The user class with variables. Need to make list variables so adding more genes (at cost of more nrg).
 # Ex. Weapons, what happens if a new weapon is evolved, developed or genes found?
 class Microbe:
-	def __init__(self, atp, type, gram, motile, weapon, quorum, chemotaxis, defense, location, special, population):
+	def __init__(self, atp, population):
 		self.atp = atp
-		self.type = type
-		self.species = species
-		self.gram = gram
-		self.motile = motile
-		self.weapon = weapon
-		self.quorum = quorum
-		self.chemotaxis = chemotaxis
-		self.defense = defense
-		self.location = location
-		self.special = special
 		self.population = population
 
 	atp = 25
-	type = "Bacteria"
-	species = ["Bacillus, Streptococcus"]
-	gram = ["Positive", "Negative"]
-	motile = False
-	weapon = ["Exotoxin - Antibiotic", "Endotoxin"]
-	quorum = False
-	chemotaxis = False
-	defense = "Pump, Enzyme"
-	location = ""
-	special = ""
 	population = 1
 
 # Methods involved in the player derived microbe class
@@ -83,38 +32,31 @@ class Microbe:
 # Changing ATP
 def metabolism(atp, mod):
 	total = atp + mod
+	print("Your new ATP is {0}", atp)
 	return total
 
+def reproduction(population):
+	population = population * 2
+	return population
+
 # Print the current characteristics
-def descibe(c):
-	print("OooOooO")
-	print("ATP amount: " + c.atp)
-	print("Your type is:" + c.type)
-	print("Your species is: " + c.species)
-	print("You are gram " + c.gram)
-	print("Is your microbe motile?" + c.motile)
-	print("What is your weapon choice?" + c.weapon)
-	print("Is quorum sensing acctive? " + c.quorum)
-	print("Do you respond to chemotaxis? "+ c.chemotaxis)
-	print("What is your defence? " + c.defense)
-	print("What is your current location? " + c.location)
-	print("What is your special ability? " + c.special)
+def descibe(atp, population):
+	print("Your current ATP is {0}", atp)
+	print("Your current Population is {0}", population)
 
 # NPC cell class to give enemies.
 class cell:
-	def __init__(self, atp, type, weapon, location, popluation):
+	def __init__(self, atp, type, weapon, location, community):
 		self.atp = atp
 		self.type = type
 		self.weapon = weapon
 		self.location = location
-		self.population = population
-
+		self.community = community
 	atp = 50
 	type = ["Macrophage", "Neutrophil", "Eosinophil", "T Cell", "B Cell"]
 	weapon = ["Ingestion", "Antibody", "MAC Attack", "Web"]
 	location = ["Organ", "Blood"]
-	population = 1
-	
+	community = 1000
 
 # Make sure you know your checks to ensure game keeps going.
 # Player has not types "q" or "quit"
@@ -125,19 +67,18 @@ class cell:
 # Do not leave things "running" in the background.
 # What currently requires ATP? Staying alive, motility, weapon, activating quorum sensing "ultimate"
 
-while True:
-	if(atp < 0):
-		break
-		print("You have run out of ATP, you have died!")
+while game_Run:
+	player_Input = ""
 	if(player_Input == "Exit" or player_Input == "exit" or player_Input == "Quit" or player_Input == "q" or player_Input == "Q"):
 		game_Run = 0
-	player_Input = input("Are you an invader or a defender? ")
-	player_input.lower()
-	if(player_Input == "invader" or player_Type == "bacteria"):
-		player_Type = player_Input
-		print("Welcome Invader! Prepare your troops.")
-	elif(player_Input == "defender" or player_Type == "Immune System"):
-		player_Type = player_Input
-		print("Welcome Defender! Prepare the defenses.")
-
+		player_Input = input("Are you an invader or a defender? ")
+		player_Input.lower()
+		if(player_Input == "invader" or player_Type == "bacteria"):
+			player_Type = player_Input
+			print("Welcome Invader! Prepare your troops.")
+		elif(player_Input == "defender" or player_Type == "Immune System"):
+			player_Type = player_Input
+			print("Welcome Defender! Prepare the defenses.")
+	# Create class with input variables.
+	user = Microbe(25, 1)
 
